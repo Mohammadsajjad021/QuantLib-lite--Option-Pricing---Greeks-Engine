898bf92a6c11ec2836edc61308dfaf136cf1bfa0
Moe <moradimohammadsajjad@gmail.com>
2026-04-09 22:28:01 +0200

    adding greeks

---
+++ src/greeks.py
@@ -0,0 +1,21 @@
+from scipy.stats import norm
+import numpy as np
+from black_scholes import d1, d2
+
+def delta_call(S, K, T, r, sigma):
+    return norm.cdf(d1(S, K, T, r, sigma))
+
+def delta_call(S, K, T, r, sigma):
+    return norm.cdf(d1(S, K, T, r, sigma)) - 1
+
+def gamma(S, K, T, r, sigma):
+    return norm.pdf(d1(S, K, T, r, sigma))/(sigma*np.sqrt(T)*S)
+
+def vega(S, K, T, r, sigma):
+    return S * norm.pdf(d1(S, K, T, r, sigma)) * np.sqrt(T)
+
+def theta(S, K, T, r, sigma):
+    return -S * norm.pdf(d1(S, K, T, r, sigma)) * sigma / (2 * np.sqrt(T)) - r * K * np.exp(-r * T) * norm.cdf(d2(S, K, T, r, sigma))
+
+def rho(S, K, T, r, sigma):
+    return K * T * np.exp(-r * T) * norm.cdf(d2(S, K, T, r, sigma))
